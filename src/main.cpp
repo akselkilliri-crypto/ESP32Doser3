13a878699889698bdad0ac77f5da731d48782cd3
akselkilliri-crypto <akselkilliri@gmail.com>
2026-03-13 11:38:35 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,52 @@
+#include <Arduino.h>
 #include <WiFi.h>
 #include <esp_wifi.h>
-#include "types.h"
-#include "serial_interface.h"
-#include "deauth.h"
 #include "definitions.h"
 
+// Global variables
 int curr_channel = 1;
+std::vector<WiFiNetwork> networks;
+std::vector<Station> stations;
 
 void setup() {
-    #ifdef LED
-    pinMode(LED, OUTPUT);
-    #endif
+    Serial.begin(115200);
+    delay(1000);
     
-    WiFi.mode(WIFI_MODE_STA);
-    start_serial_interface();
+    Serial.println("\n\n=== WiFi Deauther Starting ===");
+    
+    // Init LED
+    pinMode(LED_PIN, OUTPUT);
+    digitalWrite(LED_PIN, LOW);
+    
+    // Init WiFi
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
+    delay(100);
+    
+    // Set random MAC
+    setRandomMac();
+    
+    Serial.println("Setup complete!");
+    Serial.println("Commands:");
+    Serial.println("  scan          - Scan networks");
+    Serial.println("  show          - Show networks");
+    Serial.println("  deauth <n>    - Deauth network n (single)");
+    Serial.println("  deauth all    - Deauth all networks");
+    Serial.println("  stop          - Stop attack");
+    Serial.println("==============================");
 }
 
 void loop() {
+    // Handle serial commands
+    serial_interface_handle();
+    
+    // Channel hop if attack is active
     if (deauth_type == DEAUTH_TYPE_ALL) {
-        if (curr_channel > CHANNEL_MAX) curr_channel = 1;
-        esp_wifi_set_channel(curr_channel, WIFI_SECOND_CHAN_NONE);
         curr_channel++;
-        delay(10);
-    } else {
-        serial_interface_handle();
+        if (curr_channel > CHANNEL_MAX) curr_channel = CHANNEL_MIN;
+        esp_wifi_set_channel(curr_channel, WIFI_SECOND_CHAN_NONE);
+        delay(100);
     }
+    
+    delay(50);
 }
